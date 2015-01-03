c5a841fd924683abb1100bb108b9c2c06767d5e7
thanhphongtran24@gmail.com <503156@ELS-F19048.LA.FRD.DIRECTV.com>
2015-01-02 17:16:25 -0800

    Initial MEAN commit

---
+++ xserver.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+	console.log(req.method, req.url);
+
+	next();
+});
+
+app.use('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
+
+module.exports = app;
