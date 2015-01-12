dc7765eb039c22c7737c4f7f090a0189ec4f4d15
Phong <thanhphongtran24@gmail.com>
2015-01-12 02:06:59 -0800

    Added virtual property

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,9 +4,44 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
   firstName: String,
   lastName: String,
-  email: String,
-  username: String,
-  password: String
+  email: { 
+      type: String,
+      index: true   // Mark as secondary index
+  },
+  username: {
+	type: String,
+	trim: true,
+    unique: true    // Creates index
+  },
+  password: String,
+  created: {
+	type: Date,
+	default: Date.now
+  },
+  website: {
+	type: String,
+	get: function(url) {
+	  if (!url) {
+		return url;
+	  } else {
+		if (url.indexOf('http://') !== 0 && url.indexOf('https//') !== 0) {
+		  url = 'http://' + url;
+		}
+		return url;
+	  }
+	}
+  }
 });
 
+// Add virtual attribute to UserSchema and add a getters/setters to it 
+UserSchema.virtual('fullName').get(function() {
+    return this.firstName + ' ' + this.lastname;
+}).set(function(fullName) {
+    var splitName = fullName.split(' ');
+    this.firstName = splitName[0] || '';
+    this.lastName = splitName[1] || '';
+});
+
+//Convert MongoDB document to json representation
+UserSchema.set('toJSON', { getters: true, virtuals: true });
 mongoose.model('User', UserSchema);
