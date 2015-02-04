b019ce6ca855ed65aedb996d5bd0e9a431488e7a
Phong <thanhphongtran24@gmail.com>
2015-02-04 00:44:28 -0800

    Chapter 7

--- Angular/public/js/main.controller.js
+++ Angular/public/js/main.controller.js
@@ -105,5 +105,93 @@
         $scope.myStyle = { "background-color": 'blue' };
         $scope.days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
         $scope.msg = "Dynamic message from the model";
-    }]);
+    }])
+    .controller('customDirectiveCtrl', ['$scope', function($scope){
+        $scope.title="myApplication";
+    }])
+    .directive('myBox', function(){
+        return {
+            transclude: true,
+            restrict: 'E',
+            scope: {title: '@', bwidth: '@bwidth'},
+            template: '<div><span class="titleBar">{{title}}' +
+                '</span><div ng-transclude></div></div>',
+            link: function(scope, elem, attr, controller, transclude){
+                elem.append('<span class="footer">' + scope.$parent.title + '</span>');
+                elem.css('border', '2px ridge black');
+                elem.css('display', 'inline-block');
+                elem.css('width', scope.bwidth);
+            },
+        };
+    })
+    .directive('zoomit', function(){
+        return {
+            link: function(scope, elem, attr){
+                var dragging = false;
+                var lastX = 0;
+                elem.on('mousedown', function(event){
+                    lastX = event.pageX;
+                    event.preventDefault();
+                    dragging = true;
+                });
+                elem.on('mouseup', function(){
+                    dragging = false;
+                });
+                elem.on('mouseleave', function(){
+                    dragging = false;
+                });
+                elem.on('mousemove', function(event){
+                    if(dragging){
+                        var adjustment = null;
+                        if (event.pageX > lastX && elem.width() < 300){
+                            adjustment = 1.1;
+                        } else if (elem.width() > 100){
+                            adjustment = .9;
+                        }
+                        //requires full jQuery library
+                        if (adjustment){
+                            elem.width(elem.width()*adjustment);
+                            elem.height(elem.height()*adjustment);
+                        }
+                        lastX = event.pageX;
+                    }
+                });
+            }
+        };
+    })
+    .directive('fadeit', function(){
+        return {
+            link: function(scope, elem, attr){
+                var dragging = false;
+                var lastY = 0;
+                elem.on('mousedown', function(event){
+                    lastY = event.pageY;
+                    event.preventDefault();
+                    dragging = true;
+                });
+                elem.on('mouseup', function(){
+                    dragging = false;
+                });
+                elem.on('mouseleave', function(){
+                    dragging = false;
+                });
+                elem.on('mousemove', function(event){
+                    if (dragging){
+                        var adjustment = null;
+                        var currentOpacity = parseFloat(elem.css("opacity"));
+                        if (event.pageY > lastY && currentOpacity < 1){
+                            adjustment = 1.1;
+                        } else if (currentOpacity > 0.5){
+                            adjustment = .9;
+                        }
+                        //requires full jQuerylibrary
+                        if (adjustment){
+                            elem.css("opacity", currentOpacity*adjustment);
+                        }
+                        lastY = event.pageY;
+                    }
+                });
+            }
+        };
+    });
 })();
